29dce35b909f5e49f82ccf67625232653b39e908
pavangvernekar <pavangvernekar@gmail.com>
2025-05-16 13:32:41 +0530

    Update models.py

--- models.py
+++ models.py
@@ -7,5 +7,12 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+    def __repr__(self):
+        return f'<User {self.name} ({self.email})>'
+
     def to_dict(self):
-        return {"id": self.id, "name": self.name, "email": self.email}
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email
+        }
